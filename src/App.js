46e7e4b732d3116c0a50b1f63d296f1b03a17ea0
JingLi-olivia <jlijing@hotmail.com>
2020-06-29 09:29:38 +1000

    start

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+
+import ProgressBarContainer from './Progressbar';
+
+class App extends Component {
+    state = {
+        buttons: [],
+        bars: [],
+        limit: []
+    }
+    componentWillMount() {
+        fetch('http://pb-api.herokuapp.com/bars')
+        .then(res => res.json())
+        .then((data) => {
+        console.log('test test')
+        this.setState(
+            {
+                buttons: data.buttons,
+                bars: data.bars,
+                limit: data.limit
+            })
+        })
+        .catch(console.log)
+    }
+    render() {
+        return (
+            <div className="App">
+                <ProgressBarContainer buttons={this.state.buttons} bars={this.state.bars} limit={this.state.limit} />
+            </div>
+        );
+    }
+}
+
+export default App;
